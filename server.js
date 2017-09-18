32df20420675e5fa6008fe820a5a610e68803501
rajaraman <g.rajaraman0391@gmail.com>
2017-09-18 09:15:58 -0700

    Clean up

--- server.js
+++ server.js
@@ -3,22 +3,15 @@ var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('raja:raja@ds141474.mlab.com:41474/athlete',['athletes']);
 var bodyParser = require('body-parser');
-
-// //check route to index page
-// app.get('/', function(req, res){
-//   res.send('Hello world from server js');
-// });
 //create public folder to have static pages
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-
 //populate athlete data from mongodb using GET METHOD
 app.get('/athletes',function(req,res){
   db.athletes.find(function(err,doc){
     res.json(doc);
   });
 });
-
 //save data into mongodb POST method
 app.post('/athletes',function(req,res){
   console.log(req.body);
@@ -34,8 +27,6 @@ app.get('/athletes/:id',function(req,res){
     res.json(doc);
   });
 });
-
-
 //update athlete data in the mogodb using PUT Method
 app.put('/athletes/:id',function(req,res){
   var id = req.params.id;
