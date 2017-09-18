dcfab47985dc247e5c6e3281a34e8891162499a6
rajaraman <g.rajaraman0391@gmail.com>
2017-09-18 07:44:32 -0700

    frontend form to get athlete details updated

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,8 @@
 function AppCtrl($scope, $http) {
+
   console.log('Hello world frm controller');
+  $scope.countries = countries;
+
   $http.get('/athletes').success(function(response){
     console.log('I got the data I requested');
     $scope.athletelist = response;
