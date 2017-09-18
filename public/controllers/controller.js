b2c4dd8a213b046d9fc0cf13eed34fc18a42c7ca
rajaraman <g.rajaraman0391@gmail.com>
2017-09-18 08:01:29 -0700

    bodyparser updt POST implemented to save into mongodb

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,10 +2,19 @@ function AppCtrl($scope, $http) {
 
   console.log('Hello world frm controller');
   $scope.countries = countries;
+  $scope.sports = sports;
 
   $http.get('/athletes').success(function(response){
     console.log('I got the data I requested');
     $scope.athletelist = response;
     $scope.athlete = "";
   });
+
+//add button click controller to add athlete into mongodb
+  $scope.addAthlete = function() {
+    console.log($scope.athlete);
+    $http.post('/athletes',$scope.athlete).success(function(response){
+    console.log(response);
+    });
+  };
 }
--- server.js
+++ server.js
@@ -2,21 +2,30 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('raja:raja@ds141474.mlab.com:41474/athlete',['athletes']);
-
+var bodyParser = require('body-parser');
 
 // //check route to index page
 // app.get('/', function(req, res){
 //   res.send('Hello world from server js');
 // });
+//create public folder to have static pages
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-
+//populate athlete data from mongodb using GET METHOD
 app.get('/athletes',function(req,res){
   db.athletes.find(function(err,doc){
     res.json(doc);
   });
 });
-//create public folder to have static pages
+
+//save data into mongodb POST method
+app.post('/athletes',function(req,res){
+  console.log(req.body);
+  db.athletes.insert(req.body,function(err,doc){
+    res.json(doc);
+  });
+});
 
 app.listen(3000);
 console.log('server running on port 3000');
