2edf9f5ee2c614940a4af5b3f35f28632ca02b00
rajaraman <g.rajaraman0391@gmail.com>
2017-09-18 07:28:48 -0700

    mongodb get method to populate data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,8 @@
-function AppCtrl($scope) {
+function AppCtrl($scope, $http) {
   console.log('Hello world frm controller');
+  $http.get('/athletes').success(function(response){
+    console.log('I got the data I requested');
+    $scope.athletelist = response;
+    $scope.athlete = "";
+  });
 }
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('raja:raja@ds141474.mlab.com:41474/athlete',['athletes']);
+
 
 // //check route to index page
 // app.get('/', function(req, res){
 //   res.send('Hello world from server js');
 // });
-
-//create public folder to have static pages
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('/athletes',function(req,res){
+  db.athletes.find(function(err,doc){
+    res.json(doc);
+  });
+});
+//create public folder to have static pages
+
 app.listen(3000);
 console.log('server running on port 3000');
